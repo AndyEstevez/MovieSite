0dc7fe1937c948abc9a44584931669be61299b92
unknown <andyestevez27@gmail.com>
2020-08-18 17:20:04 -0400

    Added styling to trending films

--- src/components/TrendingFilms.js
+++ src/components/TrendingFilms.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import '../css/TrendingFilms.css';
 
 class TrendingFilms extends React.Component{
 	constructor(){
@@ -6,10 +7,8 @@ class TrendingFilms extends React.Component{
 		this.state = { trendingFilms: [] };
 	}
 
-
-
 	componentDidMount(){
-		const apikey = "API-KEY-HERE";
+		const apikey = "3522ddec15ed63abf2e2a608c6123c76";
 		const url = "https://api.themoviedb.org/3/trending/movie/week?api_key=";
 		const poster_url = "https://image.tmdb.org/t/p/w154";
 		const full_url = url + apikey;
