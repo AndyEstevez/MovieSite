9851853116d1ecce2adb58059d7d26f0801ff00c
unknown <andyestevez27@gmail.com>
2020-08-30 20:03:04 -0400

    Added recommendation section

--- src/components/MoviePage/js/Movie.js
+++ src/components/MoviePage/js/Movie.js
@@ -1,51 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import { poster_url_original, poster_url_large, movieId_url, api_key } from '../../config'; 
+import { poster_url, poster_url_large, movieId_url, similarFilms_url, api_key } from '../../config'; 
 import movieStyle from '../css/Movie.module.css'
 
 function Movie({movieId}) {
     const [Movie, setMovie] = useState([])
-
+    const [Similar, setSimilar] = useState([])
+    const [LoadingForMovie, setLoadingForMovie] = useState(true)
 
     useEffect(() => {
-        const endpoint = `${movieId_url}${movieId}?api_key=${api_key}&language=en-US`;
-        fetchMovies(endpoint)
+        const movieEndpoint = `${movieId_url}${movieId}?api_key=${api_key}&language=en-US`;
+        fetchMovies(movieEndpoint)
     }, [])
 
-    const fetchMovies = (endpoint) => {
+    const fetchMovies = (movieEndpoint) => {
+        fetch(movieEndpoint)
+            .then(details => details.json())
+            .then(details => { console.log(details)
+                setMovie(details)
 
-        fetch(endpoint)
-            .then(response => response.json())
-            .then(response => { console.log(response)
-                setMovie(response)
-            })
-        
-            console.log(endpoint);
+                const similarEndpoint = `${similarFilms_url}${movieId}/similar?api_key=${api_key}&language=en-US&page=1`;    
+                fetch(similarEndpoint)
+                    .then(similar => similar.json())
+                    .then(similar => {
+                       
+                        setSimilar(similar.results)
+                        setLoadingForMovie(false)
+                    })
            
+            })
     }
 
-        const genresJSON = Movie.genres
-        console.log(genresJSON)
-
-        // genresJSON.forEach(element => {
-        //     console.log(element.name)
-        // })
-       
+  
         return (
-            
             <div>
                 <img className={movieStyle.poster} src={`${poster_url_large}${Movie.poster_path}`} alt={Movie.title} />
-                <div className={movieStyle.title}>{Movie.title}</div>
-                <div className={movieStyle.overview}>{Movie.overview}</div>
-                <div className={movieStyle.runtime}>{Movie.runtime}m</div>
-                <div className={movieStyle.rating}>{Movie.vote_average}/10</div>
-                <div className={movieStyle.votes}>{Movie.vote_count} ratings</div>
-                <div className={movieStyle.tagline}>Tagline: {Movie.tagline}</div>
-                <div className={movieStyle.release}>{Movie.release_date}</div>
+                
+                <div className={movieStyle.container}>
+                    <div className={movieStyle.title}>{Movie.title}</div>
+                    <div className={movieStyle.overview}>{Movie.overview}</div>
+                    <div className={movieStyle.runtime}>{Movie.runtime}m</div>
+                    <div className={movieStyle.rating}>{Movie.vote_average}/10</div>
+                    <div className={movieStyle.votes}>{Movie.vote_count} ratings</div>
+                    <div className={movieStyle.tagline}>Tagline: {Movie.tagline}</div>
+                    <div className={movieStyle.release}>{Movie.release_date}</div>
+                </div>
+
+                {!LoadingForMovie ? 
+                <div>
+                    <div className={movieStyle.header}>Recommended Movies</div>
+                <div className={movieStyle.recommended}>
+                    
+                    {Similar.map(function(movie){
+                        return(
+                            <div key={movie.title}>
+                                <a href={`/movie/${movie.id}`}>
+                                <img src={`${poster_url}${movie.poster_path}`} alt={movie.title} />
+                                </a>
+                                <div>{movie.title}</div>
+                                <div>{movie.vote_average}</div>
+                                <div>{movie.release_date}</div>
+                                
+                            </div>
+                        )
+                    })}
+                </div>
+                </div>  :
+                <div></div>}
+                
+                
+                
             </div>
         )
 }
 
 
 export default Movie;
-
-
